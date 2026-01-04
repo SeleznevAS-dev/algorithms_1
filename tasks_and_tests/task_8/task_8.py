9be769ccf1d3359318db4ad02a49fc30a2d1adbe
SeleznevAS-dev <its0a0magic0time@gmail.com>
2026-01-04 05:32:49 +0300

    task 8 done

---
+++ tasks_and_tests/task_8/task_8.py
@@ -0,0 +1,46 @@
+class HashTable:
+    def __init__(self, sz, stp):
+        self.size = sz
+        self.step = stp
+        self.slots = [None] * self.size
+
+    def hash_fun(self, value):
+        value_bytes = bytearray(value, encoding="UTF-8")
+        sm = 0
+        for i in range(len(value_bytes)):
+            sm += value_bytes[i]
+        index = sm % self.size
+        return index
+
+    def seek_slot(self, value):
+        index = self.hash_fun(value)
+
+        while index < self.size:
+            if self.slots[index] is None:
+                return index
+
+            index += self.step
+
+        return None
+
+    def put(self, value):
+        index = self.hash_fun(value)
+
+        while index < self.size:
+            if self.slots[index] is None:
+                self.slots[index] = value
+                return index
+
+            index += self.step
+        return None
+
+    def find(self, value):
+        index = self.hash_fun(value)
+
+        while index < self.size:
+            if self.slots[index] == value:
+                return index
+
+            index += self.step
+
+        return None
